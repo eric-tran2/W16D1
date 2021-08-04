a7898aea401b7ec897b8dc258b345175f826de03
Eric Tran <eric.tran119@gmail.com>
2021-08-03 23:29:05 -0400

    recursive fibs

--- dsa.rb
+++ dsa.rb
@@ -128,3 +128,14 @@ def fibs(num)
 
   fibs
 end
+
+def fibs(num)
+  return [] if num == 0
+  return [0] if num == 1
+  return [0, 1] if num == 2
+
+  prev_fibs = fibs(num - 1)
+  prev_fibs << prev_fibs[-1]  + prev_fibs[-2]
+
+  prev_fibs
+end
